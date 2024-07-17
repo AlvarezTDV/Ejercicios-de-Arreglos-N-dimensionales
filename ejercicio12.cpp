7bc9627e5df41668878c96866f65572eb71a53da
Dante <dalvarezt@unjbg.edu.pe>
2024-07-16 22:54:34 -0500

    Se añade el ejercicio 12 de arreglos n dimensionales: El ejercicio trata sobre el juego de la vida de Conway en un tablero de 10x10, se debe crear de forma aleatoria el tablero, la celda puede estar viva(1) o muerta(0), se debe calcular la segunda generacion teniendo en cuenta lo siguiente: 1. Si una celula tiene menos de dos vecinos muere. 2. Una celula con dos o tres vecinos vivos sigue viva. 3. Una celula viva con mas de tres vecino muere. 4. Una celula muerta con exactamente tres vecinos vivos se convierte en una celula viva.

---
+++ ejercicio12.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <ctime>
+#include <stdlib.h>
+using namespace std;
+
+//FUNCION PARA DEFINIR LAS MINAS DE FORMA ALEATORIA
+void vidaConway( int matrizPrimeraGeneracion[][11], int matrizSegundaGeneracion[][11], int fila, int columna ) {
+	for ( int i = 0; i < fila; i++ ) {
+		for ( int j = 0; j < columna; j++ ) {
+			matrizPrimeraGeneracion[i][j] = 0;
+			matrizSegundaGeneracion[i][j] = 0;
+		}
+	}
+	for ( int i = 1; i < fila; i++ ) {
+		for ( int j = 1; j < columna; j++ ) {
+			matrizPrimeraGeneracion[i][j] = rand() % 2;
+			matrizSegundaGeneracion[i][j] = matrizPrimeraGeneracion[i][j];
+		}
+	}
+}
+
+//FUNCION PARA MOSTRAR MATRIZ
+void mostrarMatriz( int matrizPrimeraGeneracion[][11], int matrizSegundaGeneracion[][11], int fila, int columna ) {
+	for ( int i = 1; i < fila; i++ ) {
+		for ( int j = 1; j < columna; j++ ) {
+			cout << matrizPrimeraGeneracion[i][j] << " ";
+		}
+		cout << "                        ";
+		for ( int j = 1; j < columna; j++ ) {
+			cout << matrizSegundaGeneracion[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
+
+//FUNCION PARA DETECTAR MINAS
+void segundaGeneracion( int matrizPrimeraGeneracion[][11], int matrizSegundaGeneracion[][11], int fila, int columna ) {
+	for ( int i = 1; i < fila; i++ ) {
+		for ( int j = 1; j < columna; j++ ) {
+			int contador = 0;
+			if ( matrizPrimeraGeneracion[i - 1][j - 1] == 1 ) {
+				contador++;
+			}
+			if ( matrizPrimeraGeneracion[i - 1][j] == 1 ) {
+				contador++;
+			} 
+			if ( matrizPrimeraGeneracion[i - 1][j + 1] == 1 ) {
+				contador++;
+			} 
+			if ( matrizPrimeraGeneracion[i][j - 1] == 1 ) {
+				contador++;
+			} 
+			if ( matrizPrimeraGeneracion[i][j + 1] == 1 ) {
+				contador++;
+			} 
+			if ( matrizPrimeraGeneracion[i + 1][j - 1] == 1 ) {
+				contador++;
+			} 
+			if ( matrizPrimeraGeneracion[i + 1][j] == 1 ) {
+				contador++;
+			} 
+			if ( matrizPrimeraGeneracion[i + 1][j + 1] == 1 ) {
+				contador++;
+			}
+			if ( contador < 2 ) {
+				matrizSegundaGeneracion[i][j] = 0;
+			} else if ( contador == 2 && contador == 3 ) {
+				matrizSegundaGeneracion[i][j] = 1;
+			} else if ( contador > 3 ) {
+				matrizSegundaGeneracion[i][j] = 0;
+			} else if ( matrizPrimeraGeneracion[i][j] == 0) {
+				if ( contador == 3 ) {
+					matrizSegundaGeneracion[i][j] = 1;
+				}
+			}
+		}
+	}
+}
+
+int main() {
+	srand(time(0));
+	int matrizPrimeraGeneracion[11][11];
+	int matrizSegundaGeneracion[11][11];
+	vidaConway(matrizPrimeraGeneracion, matrizSegundaGeneracion, 11, 11);
+	segundaGeneracion(matrizPrimeraGeneracion, matrizSegundaGeneracion, 11, 11);
+	cout << "Primera generacion:                       Segunda generacion: " << endl;
+	mostrarMatriz(matrizPrimeraGeneracion, matrizSegundaGeneracion, 11, 11);
+	return 0;
+}
